2a7191942f5d5084210ec3c7c638eaf6c6435591
NinjaLabs <fraser@ninjalabs.co.uk>
2024-05-22 22:02:12 +0100

    Allow access for filament admin users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@ninjalabs.dev');
+    }
 }
