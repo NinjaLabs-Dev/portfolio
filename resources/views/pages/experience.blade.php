5625459e5e032c63ba811ea29be7a0b3ce17ae5f
NinjaLabs <fraser@ninjalabs.co.uk>
2024-05-22 23:57:10 +0100

    Update experience image handling

--- resources/views/pages/experience.blade.php
+++ resources/views/pages/experience.blade.php
@@ -53,45 +53,38 @@
             </div>
 
             @foreach($projects as $project)
-                <div class="2xl:col-span-4 2xl:col-start-8 col-span-12 flex justify-between group items-center gap-4 p-6 border-4 rounded-lg shadow bg-[#111] border-[#222]">
-                    <div class="flex justify-between gap-4 w-full">
-                        <div class="flex flex-col text-left">
-                            <div class="flex justify-between items-center">
-                                @if($project->project_url)
-                                    <a href="{{ $project->project_url }}" target="_blank" nofollow class="flex items-center gap-2 underline hover:opacity-80 transition-all">
-                                        <h1 class="text-2xl font-bold">{{ $project->name }}</h1>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                            <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clip-rule="evenodd" />
-                                            <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clip-rule="evenodd" />
-                                        </svg>
-                                    </a>
-                                @else
-                                    <div class="flex items-center gap-2">
-                                        <h1 class="text-2xl font-bold">{{ $project->name }}</h1>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                            <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                @endif
+                <div class="flex flex-col items-center justify-between bg-[#111] border-4 border-[#222] rounded-lg shadow md:flex-row overflow-hidden">
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        @if($project->project_url)
+                            <a href="{{ $project->project_url }}" target="_blank" nofollow class="flex items-center gap-2 underline hover:opacity-80 transition-all">
+                                <h1 class="text-2xl font-bold">{{ $project->name }}</h1>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                    <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clip-rule="evenodd" />
+                                    <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+                        @else
+                            <div class="flex items-center gap-2">
+                                <h1 class="text-2xl font-bold">{{ $project->name }}</h1>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
+                                </svg>
                             </div>
-                            @if($project->languages)
-                                <div class="mt-2">
-                                    @foreach($project->languages as $language)
-                                        <span class="text-xs font-medium me-2 px-2.5 py-0.5 rounded bg-[#333] text-zinc-300">{{ $language }}</span>
-                                    @endforeach
-                                </div>
-                            @endif
+                        @endif
 
-                            <p class="mt-2">{{ $project->overview }}</p>
-                        </div>
-                        <div class="hidden lg:block">
-                            @if($project->snapshot)
-                                <a href="{{ asset('/storage/' . $project->snapshot) }}" target="_blank">
-                                    <img src="{{ asset('/storage/' . $project->snapshot) }}" alt="{{ $project->name }} Snapshop Picture" class="w-[20vh] rounded-md">
-                                </a>
-                            @endif
-                        </div>
+                        @if($project->languages)
+                            <div class="mt-2">
+                                @foreach($project->languages as $language)
+                                    <span class="text-xs font-medium me-2 px-2.5 py-0.5 rounded bg-[#333] text-zinc-300">{{ $language }}</span>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        <p class="mt-2">{{ $project->overview }}</p>
                     </div>
+                    @if($project->snapshot)
+                        <img class="object-cover rounded-t-lg h-full md:w-48 md:rounded-none" src="{{ asset('/storage/' . $project->snapshot) }}" alt="">
+                    @endif
                 </div>
             @endforeach
 
