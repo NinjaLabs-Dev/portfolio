9d345e57a0be164721c3c94d01eb422c1f69b809
NinjaLabs <fraser@ninjalabs.co.uk>
2024-05-23 00:05:27 +0100

    Add Discord URL redirect

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/experience', [ExperienceController::class, 'index'])->name('experience');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+Route::domain('discord')->group(static function() {
+    Route::redirect('/', 'https://discord.gg/FnNXxAn');
+});
